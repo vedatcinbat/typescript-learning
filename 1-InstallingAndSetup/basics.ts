e69438f0bbbf9e146127c71bbb74cc5a96b93520
vedat cinbat <cinbatvedat@gmail.com>
2024-07-06 02:13:44 +0300

    2-InstallingAndSetup notes updated and basics.ts file added

---
+++ 1-InstallingAndSetup/basics.ts
@@ -0,0 +1,8 @@
+console.log("Hello World");
+console.log("GOOODBYE!!!")
+console.log(7.34525423566)
+
+/*
+Math.round(3)
+//Math.round("Hello") // Error: Argument of type '"Hello"' is not assignable to parameter of type 'number'.
+*/
