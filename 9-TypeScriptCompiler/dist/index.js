2e7d43510271e999999f57fe6e03d24841a49af9
vedat cinbat <cinbatvedat@gmail.com>
2024-07-11 08:11:20 +0300

    9-TypescriptCompiler Notes Taken and Practiced

---
+++ 9-TypeScriptCompiler/dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+const norma = {
+    breed: 'Rhode Island Red',
+    eggsPerWeek: 5,
+    name: 'Norma',
+    age: 2
+};
+const juniper = {
+    breed: 'Buff Orpington',
+    eggsPerWeek: 3,
+    name: 'Juniper',
+    age: 1
+};
+console.log(norma);
